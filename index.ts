a65e71231630ebe542981b88e95e53a2bc4a4c56
Sabahat Shakeel <159911882+sabahat97@users.noreply.github.com>
2024-05-16 19:10:41 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+const myProperties = {
+    name :"sabahat",
+    age : 10,
+    rollNo:787,
+    adress:"abc adress"
+    
+    
+    }
+    //console.log (myProperties)
+    //console.log (myProperties.age) //dot notation , dynamically
+    console.log(myProperties["age"])//array notation ,square bracket
+    const userPrompt= "name";
+    console.log(myProperties[userPrompt])
